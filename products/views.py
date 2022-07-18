cc9cd6897e1abca86fa6c4e0920a3fec8cebdad7
JavierBL89 <notri80@gmail.com>
2022-07-18 23:03:14 +0000

    Added product list images, description and product details on product details pages

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product, Size
+from .models import Product, Size, Image, ProductDetail
 # Create your views here.
 
 
@@ -19,8 +19,25 @@ def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
     size_list = Size.objects.filter(product_id=product_id).all()
+    images_list = Image.objects.filter(product_id=product_id).all()
+    product_details= ProductDetail.objects.filter(product_id=product_id).all()
+    
+    # loop through images_list dict object
+    for image in images_list.values():
+        images_list = list(image.values())
+        images_list = images_list[2:]
+    
+    # loop through product details dict object
+    for detail in product_details.values():
+        # product_details = list(detail)
+        details_key = list(detail)[2:]
+        details_value = list(detail.values())[2:]
+        # print(product_details)
     context = {
         'product': product,
-        'size_list': size_list
+        'size_list': size_list,
+        'images_list': images_list,
+        'details_key': details_key,
+        'details_value': details_value
     }
     return render(request, 'products/product_detail.html', context)
