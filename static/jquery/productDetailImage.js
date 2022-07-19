f6c9b38494d87bfa74cfc7314f79a02da64f7c07
JavierBL89 <notri80@gmail.com>
2022-07-19 10:43:54 +0000

    Displayin product list images when clicking on product detail page

---
+++ static/jquery/productDetailImage.js
@@ -0,0 +1,21 @@
+/* Ddocument onload */
+
+
+jQuery(function(){
+    
+    list = document.getElementsByClassName("puta").length
+
+    for(var i = 0; i < list; i++){
+        console.log(i);
+
+    }
+
+    // for (image of list){
+    //     console.log(image[i]);
+    // }
+    // $("#product-image-list").on("click", function(){
+    //     for e in
+    //     console.log("puta");
+    // newImage = $(".product-image-list").getElementByClassName("product-image-list");
+    // });
+})
