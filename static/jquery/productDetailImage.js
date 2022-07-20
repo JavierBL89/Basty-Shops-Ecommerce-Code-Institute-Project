871a41056f448baf32da3e8cd609e7c8c8fd0efa
JavierBL89 <notri80@gmail.com>
2022-07-20 16:11:11 +0000

    Added multiple product images functionality

--- static/jquery/productDetailImage.js
+++ static/jquery/productDetailImage.js
@@ -3,19 +3,11 @@
 
 jQuery(function(){
     
-    list = document.getElementsByClassName("puta").length
-
-    for(var i = 0; i < list; i++){
-        console.log(i);
-
-    }
-
-    // for (image of list){
-    //     console.log(image[i]);
-    // }
-    // $("#product-image-list").on("click", function(){
-    //     for e in
-    //     console.log("puta");
-    // newImage = $(".product-image-list").getElementByClassName("product-image-list");
-    // });
-})
+    list = $(".product-image");
+    list.on("click", function(){
+        newImage = this.getAttribute("src")
+        mainImage = $("img")[0].setAttribute("src", newImage)
+        console.log(mainImage);
+    });
+    
+});
