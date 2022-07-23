a5b2f3b8583e264d5ffae894fde4611cc12eb35b
JavierBL89 <notri80@gmail.com>
2022-07-23 16:29:17 +0000

    Added searching products by name, title or description

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product, Size, Image, ProductDetail
+from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 
@@ -8,8 +10,28 @@ def all_products(request):
     including sorting and searching queries """
 
     products = Product.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category']
+            if not categories:
+                messages.error(request, "No category found")
+                return redirect(reverse, ('products'))
+            category = Q(name__icontains=categories)
+            products = products.filter(category)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search criteria")
+                return redirect(reverse, ('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(title__icontains=query)
+            products = products.filter(queries)
+
     context = {
-        'products': products
+        'products': products,
+        'search_term': query
     }
     return render(request, 'products/products.html', context)
 
