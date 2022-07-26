6186ffbdda577f6a342cb2ed19d2eca738835e59
JavierBL89 <notri80@gmail.com>
2022-07-26 16:39:42 +0000

    Added function add_to_bag

---
+++ bag/contexts.py
@@ -0,0 +1,37 @@
+from django.shortcuts import get_object_or_404
+from decimal import Decimal
+from django.conf import settings
+from products.models import Product
+
+
+def bag_contents(request):
+    bag_items = []
+    total = 0
+    product_count = 0
+    delivery = 0
+
+    bag = request.session.get('bag', {})
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+    
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product
+            })
+        print(bag_items)
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.DELIVERY_PERCENTAGE/100)
+    
+    grand_total = delivery + total
+    context = {
+        'bag_items': bag_items,
+        'product_count': product_count,
+        'delivery': delivery,
+        'total': total,
+        'grand_total': grand_total,
+    }
+    return context
--- basty_shops/settings.py
+++ basty_shops/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents'
             ],
         },
     },
@@ -165,3 +166,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+DELIVERY_PERCENTAGE = 10
