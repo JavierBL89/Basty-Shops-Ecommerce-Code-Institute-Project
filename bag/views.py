b3f4afd5dbb6c31fd3e511e20cfe371fac9b6e6d
JavierBL89 <notri80@gmail.com>
2022-08-05 22:03:55 +0000

    Increment and decrement buttons in bag page now working

--- bag/views.py
+++ bag/views.py
@@ -41,40 +41,42 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    print('puta')
-    quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
+    pass
+#     print('puta')
+#     quantity = int(request.POST.get('quantity'))
+#     size = None
+#     if 'product_size' in request.POST:
+#         size = request.POST['product_size']
 
-    bag = request.session.get('bag', {})
-    print(bag)
-    if size:
-        if quantity > 0:
-            bag[item_id]['item_size']['quantity'] = quantity
-        else:
-            del bag[item_id]['item_size']['quantity']
-            if not bag[item_id]['item_size']['quantity']:
-                bag.pop(item_id)
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+#     bag = request.session.get('bag', {})
+#     print(bag)
+#     if size:
+#         if quantity > 0:
+#             bag[item_id]['item_size']['quantity'] = quantity
+#         else:
+#             del bag[item_id]['item_size']['quantity']
+#             if not bag[item_id]['item_size']['quantity']:
+#                 bag.pop(item_id)
+#     request.session['bag'] = bag
+#     return redirect(reverse('view_bag'))
 
 
 def remove_item(request, item_id):
-    try:
-        # size = None 
-        # if 'size' in request.POST:
-        #     size =  request.POST.get['size']
-        bag = request.session.get('bag', {})
-        # if size:
-            # del bag[item_id]['item_size']['size']
-            # if not bag[item_id]['item_size']['size']
-            #     bag.pop(item_id)
-        # else:
-        bag.pop(item_id)
-        request.session['bag'] = bag
-        print(request.session['bag'], 'bag session')
-        return HttpResponse(status=200)
+    pass
+#     try:
+#         # size = None 
+#         # if 'size' in request.POST:
+#         #     size =  request.POST.get['size']
+#         bag = request.session.get('bag', {})
+#         # if size:
+#             # del bag[item_id]['item_size']['size']
+#             # if not bag[item_id]['item_size']['size']
+#             #     bag.pop(item_id)
+#         # else:
+#         bag.pop(item_id)
+#         request.session['bag'] = bag
+#         print(request.session['bag'], 'bag session')
+#         return HttpResponse(status=200)
 
-    except Exception as e:
-        return HttpResponse(status=500)
+#     except Exception as e:
+#         return HttpResponse(status=500)
