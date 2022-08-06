b765c55c27513f6c84a083ab351980798078703a
JavierBL89 <notri80@gmail.com>
2022-08-06 19:54:59 +0000

    Added handle_stock function

--- bag/handle_stock.py
+++ bag/handle_stock.py
@@ -4,9 +4,23 @@ from products.models import Product, Size
 def handle_stock(item_size_id):
 
     data_item = Size.objects.filter(id=item_size_id).all()
-
+    
+    # print(stock)
     for data in data_item.values():
-        # data_item[data]['stock'] -= 1
+
+        product_id = data['product_id_id']
+        size_id = data['id']
+        size = data['size']
+        size_stock = data['stock'] - 1
+
+        new_stock = Size(id=size_id, product_id_id=product_id, size=size, stock=size_stock)
+        new_stock.save()
+        print(data, 'dataaaaa')
+
+        # print(new_stock, 'putaaaa')
+        # print(data, 'dataaaaa')
+        # stock = data_item(stock=puta)
+        # stock.save()
         # print(data['stock'], 'dataaaaa')
         # data['stock'] -= 1
         # if data['stock'] >= 0:
