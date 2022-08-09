8704ec9a34e4bd4da249a287527ff9be627f7d3a
JavierBL89 <notri80@gmail.com>
2022-08-09 14:15:21 +0000

    Created signals.py file in checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -14,7 +14,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=250, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    coutry = models.CharField(max_length=40, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
     post_code = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address_1 = models.CharField(max_length=80, null=True, blank=False)
