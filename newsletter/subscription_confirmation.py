3ea9ea8318360fb14729d6f809892c5f4073b1de
JavierBL89 <notri80@gmail.com>
2022-09-13 11:26:28 +0000

    Make migrations into heroku solved Error when subscribing to the newsletter

--- newsletter/subscription_confirmation.py
+++ newsletter/subscription_confirmation.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 def send_subs_confirm_email(new_member):
     """Send the user a confirmation email"""
-    print('puta')
     cust_email = new_member.email
     subject = render_to_string(
         'newsletter/subs_confir_email/confirmation_email_subject.txt',
@@ -14,9 +13,10 @@ def send_subs_confirm_email(new_member):
         'newsletter/subs_confir_email/confirmation_email_body.txt',
         {'new_member': new_member,
          'contact_email': settings.DEFAULT_FROM_EMAIL})
+    
     send_mail(
         subject,
         body,
         settings.DEFAULT_FROM_EMAIL,
-        [cust_email],
+        [cust_email]
     )
